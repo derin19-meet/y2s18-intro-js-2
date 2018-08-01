b6791f2739b99216bd8cadbe6c72ad32c0adea64
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:11:21 +0300

    js part2

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,13 @@
 // Create your 'me' object literal here!
+
+
+var me={
+	firstName:"derin",
+	lastName:"dovek",
+	favoriteFoods: ["grapes" , "sushi"],
+	age: 17,
+	picture:"https://www.google.com/search?q=dogs&client=ubuntu&hs=K9q&channel=fs&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi6q_6D0cvcAhWEJlAKHRSEBMcQ_AUICigB&biw=1156&bih=553#imgrc=vXX1i3KCrzmURM:"	
+}
+
+var me_json = JSON.stringify(me)
+document.write(me_json)
--- separation/code.js
+++ separation/code.js
@@ -1,1 +1,4 @@
 // Add your alert() here!
+
+
+alert ("derin")
